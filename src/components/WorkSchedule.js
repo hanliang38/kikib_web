bb3de4e8f89b18101239da03dadf50f75e22b73b
mira <mira@miraui-MacBookPro.local>
2021-12-27 17:40:34 +0900

    distinguish work, leave

--- src/components/WorkSchedule.js
+++ src/components/WorkSchedule.js
@@ -7,7 +7,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import axios from 'axios';
 import loadingImg from '../assets/Drawables/icon_progressbar_onloading.png';
-import { icons } from 'react-icons/lib';
+import { MdWork } from 'react-icons/md';
 
 axios.withCredentials = true;
 axios.defaults.withCredentials = true;
@@ -23,12 +23,15 @@ let mon = ('0' + (today.getMonth() + 1)).slice(-2);
 // console.log('현재' + { year_month } + '입니다');
 let currentMonth = year + '-' + mon;
 
+console.log('currentMonth::', currentMonth);
+
 const WorkSchedule = () => {
   // 근무일 수
   const [workDatas, setWorkDatas] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [leaveDatas, setLeaveDatas] = useState(null);
+  // const [count, setCount] = useState(0);
 
   // 근무일 데이터를 구하기
   const workDay = async () => {
@@ -78,9 +81,26 @@ const WorkSchedule = () => {
   // 근무일 수
   let day_num = wdatas.length;
   // 근무일 날짜
-  // let workDays = wdatas.map((value) => {
-  //   return value.date;
-  // });
+  let workDays = wdatas.map((value) => {
+    return value.date;
+  });
+  // console.log(workDays);
+
+  let events = () => {
+    for (let i in workDays.length) {
+      for (let j in workDays.length) {
+        let WorkArr = [];
+        j = {};
+        j.title = 'work';
+        j.date = workDays[i];
+        j.img = { MdWork };
+
+        return WorkArr.push(j);
+      }
+    }
+  };
+  events();
+  console.log('events::', events());
 
   // 휴무일 obj
   let ldatas = leaveDatas.object;
@@ -92,6 +112,14 @@ const WorkSchedule = () => {
     return value.date;
   });
   console.log('leaveDays::', leaveDays);
+
+  // 이벤트 아이콘 호출하는 함수
+  function EventContent(eventInfo) {
+    <>
+      <i>{eventInfo.event.icon}</i>
+    </>;
+    console.log(eventInfo);
+  }
 
   return (
     <div>
@@ -106,7 +134,45 @@ const WorkSchedule = () => {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay',
           }}
-          events={[{ title: 'work', date: '2021-12-25' }]}
+          eventContent={EventContent}
+          eventSources={[
+            {
+              events: [
+                { title: '근무', date: '2021-12-07', icon: <MdWork /> },
+                { title: '근무', date: '2021-12-09', icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-11' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-13' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-15' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-17' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-19' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-21' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-23' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-25' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-27' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-29' ,icon: <MdWork /> },
+                // { title: '근무', date: '2021-12-31' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-02' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-04' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-06' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-08' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-10' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-12' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-14' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-16' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-18' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-20' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-22' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-24' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-26' ,icon: <MdWork /> },
+                // { title: '근무', date: '2022-01-30' ,icon: <MdWork /> },
+              ],
+              color: 'green',
+            },
+            {
+              events: [{ title: '휴무', date: '2021-12-25' }],
+              color: 'red',
+            },
+          ]}
           locale="ko"
         />
         <div className="leave-work-table">
