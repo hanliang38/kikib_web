8e3bc532d2403402116a0fde431bd047300fafd9
mira <mira@miraui-MacBookPro.local>
2022-01-01 22:15:06 +0900

    refresh error & button click error

--- src/components/WorkSchedule.js
+++ src/components/WorkSchedule.js
@@ -13,170 +13,185 @@ import { useLocation, Navigate } from 'react-router-dom';
 axios.withCredentials = true;
 axios.defaults.withCredentials = true;
 
+const today = new Date();
+const nowYear = today.getFullYear();
+const nowMonth = today.getMonth() + 1;
+const nowYearMonth = nowYear + '-' + nowMonth;
+
 const WorkSchedule = () => {
   const location = useLocation();
+  let userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
 
   // 근무일 수
-  const [workDatas, setWorkDatas] = useState(null);
+  const [workData, setWorkData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [dayNum, setDayNum] = useState(0);
+  const [leaveNum, setLeaveNum] = useState(0);
+  const [allEvents, setAllEvents] = useState([]);
+  const [currentYearMonth, setCurrentYearMonth] = useState(nowYearMonth);
+
   // const [monthChange, setMonthChange] = useState('')
-  // const [count, setCount] = useState(0);
+  // const [count, setCount] = useState();
 
   useEffect(() => {
-    workDay();
+    fetchData(currentYearMonth);
+  }, []);
+
+  useEffect(() => {
+    // obj 분할 (array) status ==> work, work-check, leave, leave-check
+    console.log('workData::', workData);
+    console.log('workData.status::', workData);
+    // 근무일
+    const workDays = workData
+      .filter((item) => item.status === 'WORK')
+      .map((workDay) => {
+        return {
+          date: workDay.date,
+          title: '근무',
+          color: 'blue',
+          url: 'MdWork',
+        };
+      });
+    // console.log(workDays, 'workDays');
+
+    // 근무확인
+    const workCheckDays = workData
+      .filter((item) => item.status === 'WORK-CHECK')
+      .map((workCheckDay) => {
+        return {
+          date: workCheckDay.date,
+          title: '근무',
+          color: 'blue',
+          url: 'MdWork',
+        };
+      });
+    // console.log(workCheckDays, 'workCheckDays');
+
+    // 휴무일
+    const leaveDays = workData
+      .filter((item) => item.status === 'LEAVE')
+      .map((leaveDay) => {
+        return {
+          date: leaveDay.date,
+          title: '휴무',
+          color: 'red',
+        };
+      });
+    // console.log(leaveDays, 'leaveDays');
+
+    // 휴무확인
+    const leaveCheckDays = workData
+      .filter((item) => item.status === 'LEAVE-CHECK')
+      .map((leaveCheckDay) => {
+        return {
+          date: leaveCheckDay.date,
+          title: '휴무',
+          color: 'red',
+        };
+      });
+    // console.log(leaveCheckDays, 'leaveCheckDays');
+
+    const annualDays = workData
+      .filter((item) => item.status === 'ANNUAL')
+      .map((annualDay) => {
+        return {
+          date: annualDay.date,
+          title: '연차',
+          color: 'red',
+        };
+      });
+    // console.log(annualDays, 'annualDays');
+
+    const annualCheckDays = workData
+      .filter((item) => item.status === 'ANNUAL-CHECK')
+      .map((annualCheckDay) => {
+        return {
+          date: annualCheckDay.date,
+          title: '연차',
+          color: 'red',
+        };
+      });
+    // console.log(annualCheckDays, 'annualCheckDays');
+    // 근무일 수 (work + work-check)
+    setDayNum([...workDays, ...workCheckDays].length);
+
+    // 휴무일 수 (leave + leave-check + annual + annual-check)
+    setLeaveNum(
+      [...leaveDays, ...leaveCheckDays, ...annualDays, ...annualCheckDays]
+        .length
+    );
+
+    // 모든 날짜 이벤트
+    setAllEvents([
+      ...workDays,
+      ...workCheckDays,
+      ...leaveDays,
+      ...leaveCheckDays,
+      ...annualDays,
+      ...annualCheckDays,
+    ]);
+    // console.log(workData);
+    // console.log('workEvents::', workEvents);
   }, []);
 
   // if (sessionStorage.getItem('userInfo') === null) {
   //   return navigate('/');
   // }
 
-  let userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
-
   if (!userInfo) {
     // 없을 때
     return <Navigate to="/" state={{ from: location }} replace />;
   }
 
-  // console.log(userdata);
   const { userId } = userInfo;
-  // console.log('userId: ', userId);
-  const currentMonth = '2022-01';
-  console.log('currentMonth::', currentMonth);
 
-  // 근무일 데이터를 구하기
-  const workDay = async () => {
+  const fetchData = async () => {
     try {
       setError(null);
-      setWorkDatas(null);
       setLoading(true);
+      setCurrentYearMonth(nowYearMonth);
       const response = await axios.get(
-        `/api/driver/${userId}?yearMonth=${currentMonth}`
+        `/api/driver/${userId}?yearMonth=${currentYearMonth}`
       );
-      setWorkDatas(response.data);
+      let res = response.data.object;
+      // console.log(res);
+      setWorkData(res);
     } catch (e) {
       setError(e);
     }
     setLoading(false);
   };
 
+  // console.log('userId: ', userId);
+  // 근무일 데이터를 구하기
+
   if (loading) return <div>{loadingImg}</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
-  if (!workDatas) return null;
+  if (!workData) return null;
 
-  // console.log('workDatas::', workDatas);
-
-  // obj 분할 (array) status ==> work, work-check, leave, leave-check
-  let objs = workDatas.object;
-  // console.log('objs::', objs);
-
-  // 근무일
-  const workDays = objs
-    .filter((item) => item.status === 'WORK')
-    .map((workDay) => {
-      return {
-        date: workDay.date,
-        title: '근무',
-        color: 'blue',
-        url: 'MdWork',
-      };
-    });
-  // console.log(workDays, 'workDays');
-
-  // 근무확인
-  const workCheckDays = objs
-    .filter((item) => item.status === 'WORK-CHECK')
-    .map((workCheckDay) => {
-      return {
-        date: workCheckDay.date,
-        title: '근무',
-        color: 'blue',
-        url: 'MdWork',
-      };
-    });
-  // console.log(workCheckDays, 'workCheckDays');
-
-  // 휴무일
-  const leaveDays = objs
-    .filter((item) => item.status === 'LEAVE')
-    .map((leaveDay) => {
-      return {
-        date: leaveDay.date,
-        title: '휴무',
-        color: 'red',
-      };
-    });
-  // console.log(leaveDays, 'leaveDays');
-
-  // 휴무확인
-  const leaveCheckDays = objs
-    .filter((item) => item.status === 'LEAVE-CHECK')
-    .map((leaveCheckDay) => {
-      return {
-        date: leaveCheckDay.date,
-        title: '휴무',
-        color: 'red',
-      };
-    });
-  // console.log(leaveCheckDays, 'leaveCheckDays');
-
-  const annualDays = objs
-    .filter((item) => item.status === 'ANNUAL')
-    .map((annualDay) => {
-      return {
-        date: annualDay.date,
-        title: '연차',
-        color: 'red',
-      };
-    });
-  // console.log(annualDays, 'annualDays');
-
-  const annualCheckDays = objs
-    .filter((item) => item.status === 'ANNUAL-CHECK')
-    .map((annualCheckDay) => {
-      return {
-        date: annualCheckDay.date,
-        title: '연차',
-        color: 'red',
-      };
-    });
-  // console.log(annualCheckDays, 'annualCheckDays');
-  // 근무일 수 (work + work-check)
-  let day_num = [...workDays, ...workCheckDays].length;
-
-  // 휴무일 수 (leave + leave-check + annual + annual-check)
-  let leave_num = [
-    ...leaveDays,
-    ...leaveCheckDays,
-    ...annualDays,
-    ...annualCheckDays,
-  ].length;
-
-  // 모든 날짜 이벤트
-  let allEvents = [
-    ...workDays,
-    ...workCheckDays,
-    ...leaveDays,
-    ...leaveCheckDays,
-    ...annualDays,
-    ...annualCheckDays,
-  ];
-  // console.log('workEvents::', workEvents);
+  // console.log('workData::', workData);
 
   // // 이벤트 아이콘 호출하는 함수
   // function renderEventContent(eventInfo) {
   //   return {eventInfo.event.url === "MdWork" ? <MdWork /> : <></>};
   // }
 
-  const handleMonthChange = (e) => {
-    const currentDate = e.view.getCurrentData().currentDate;
+  const handleMonthChange = async (e) => {
+    try {
+      const currentDate = e.view.getCurrentData().currentDate;
 
-    if (currentDate instanceof Date) {
-      const year = currentDate.getFullYear();
-      const month = currentDate.getMonth() + 1;
-      const dateString = `${year}-${month}`;
+      if (currentDate instanceof Date) {
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth() + 1;
+        const dateString = `${year}-${month}`;
 
-      console.log('##', dateString);
+        console.log('##', dateString);
+        // fetchData(dateString);
+        setCurrentYearMonth(dateString);
+      }
+    } catch (e) {
+      console.error();
     }
   };
 
@@ -205,8 +220,8 @@ const WorkSchedule = () => {
                 <th>휴무일</th>
               </tr>
               <tr>
-                <th>{day_num}일</th>
-                <th>{leave_num}일</th>
+                <th>{dayNum}일</th>
+                <th>{leaveNum}일</th>
               </tr>
             </tbody>
           </table>
