c777041a3226baa58ebce45c2085b19ecf85c89a
박선희 <sssssunny_p@naver.com>
2022-02-07 16:07:39 +0900

    메인 - 이미지 교체, css 내 페이지 클래스 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,7 @@ import React, { useState, useLayoutEffect } from 'react';
 import logoHeader from '../assets/img/logo_header.png';
 import icoSearch from '../assets/img/ico_search.png';
 import icoMsg from '../assets/img/ico_msg.png';
-import logoBus from '../assets/img/logo_bus.svg';
+import logoBus from '../assets/img/logo_bus.png';
 import Clock from 'react-live-clock';
 import 'moment/locale/ko';
 import { useLocation, Navigate } from 'react-router';
@@ -75,203 +75,205 @@ const Main = () => {
   }
 
   return (
-    <div className="container main">
-      <header>
-        <div className="logo">
-          <a href="/main">
-            <img src={logoHeader} alt="kikiB" />
-          </a>
-        </div>
-        <p className="page-title">메인화면</p>
-        <div className="btn-box">
-          <a href="#!" className="btn-search">
-            <img src={icoSearch} alt="검색" />
-          </a>
-          <a href="#!" className="btn-msg">
-            <img src={icoMsg} alt="메시지" />
-          </a>
-        </div>
-      </header>
-
-      <div className="user-box">
-        {/* <h1>{userName} {error ? `?` : busRouteData}번 승무원님</h1> //디자인 버스번호 없어서 주석처리 */}
-        <div className="logo">
-          <img src={logoBus} alt="" />
-        </div>
-        <h1>
-          <span>삼영운수</span> {userName}님
-        </h1>
-        <a href="#!" className="btn-timetable">
-          출근부
-        </a>
-      </div>
-
-      <div className="inner">
-        <div className="daily-box">
-          <div className="box-inner">
-            <div className="item date">
-              <Clock
-                className="first"
-                format={'MM월DD일'}
-                ticking={true}
-                timezone={'Asia/Seoul'}
-              />
-              <Clock
-                className="second"
-                format={'dddd'}
-                ticking={true}
-                timezone={'Asia/Seoul'}
-              />
-            </div>
-            <div className="item clock">
-              <Clock
-                className="first"
-                format={'A'}
-                ticking={true}
-                timezone={'Asia/Seoul'}
-                locale="en"
-              />
-              <Clock
-                className="second"
-                format={'hh:mm'}
-                ticking={true}
-                timezone={'Asia/Seoul'}
-              />
-            </div>
-            <div className="item weather ico-weather01">-3°C</div>
+    <>
+      <div className="container main">
+        <header>
+          <div className="logo">
+            <a href="/main">
+              <img src={logoHeader} alt="kikiB" />
+            </a>
           </div>
+          <p className="page-title">메인화면</p>
+          <div className="btn-box">
+            <a href="#!" className="btn-search">
+              <img src={icoSearch} alt="검색" />
+            </a>
+            <a href="#!" className="btn-msg">
+              <img src={icoMsg} alt="메시지" />
+            </a>
+          </div>
+        </header>
+
+        <div className="user-box">
+          {/* <h1>{userName} {error ? `?` : busRouteData}번 승무원님</h1> //디자인 버스번호 없어서 주석처리 */}
+          <div className="logo">
+            <img src={logoBus} alt="" />
+          </div>
+          <h1>
+            <span>삼영운수</span>&nbsp;{userName}님
+          </h1>
+          <a href="#!" className="btn-timetable">
+            출근부
+          </a>
         </div>
 
-        {/* 
-          1. .btn-common에 추가
-            1) .btn-disabled : 버튼 텍스트 회색 처리, 준비중 추가
-            2) .btn-holiday : 휴무신청기간 추가
-        */}
-        <ul className="menu-list">
-          <li className="type-cell">
-            <a
-              href="/management"
-              className="btn-common"
-              component={busRouteData}
-            >
-              <span className="menu-title">
-                근무
-                <br />
-                일정
-              </span>
-
-              <div className="icon-box">
-                <i className="ico-alarm on"></i>
-                <i className="ico-calendar"></i>
+        <div className="inner">
+          <div className="daily-box">
+            <div className="box-inner">
+              <div className="item date">
+                <Clock
+                  className="first"
+                  format={'MM월DD일'}
+                  ticking={true}
+                  timezone={'Asia/Seoul'}
+                />
+                <Clock
+                  className="second"
+                  format={'dddd'}
+                  ticking={true}
+                  timezone={'Asia/Seoul'}
+                />
               </div>
-            </a>
-          </li>
-          <li className="type-cell">
-            <a
-              href="/personalTimeTable"
-              className="btn-common"
-              // onClick={() => alert('준비중인 기능입니다.')}
-            >
-              <span className="menu-title">
-                배차
-                <br />
-                일보
-              </span>
-
-              <div className="icon-box">
-                <i className="ico-alarm"></i>
-                <i className="ico-bus-run"></i>
+              <div className="item clock">
+                <Clock
+                  className="first"
+                  format={'A'}
+                  ticking={true}
+                  timezone={'Asia/Seoul'}
+                  locale="en"
+                />
+                <Clock
+                  className="second"
+                  format={'hh:mm'}
+                  ticking={true}
+                  timezone={'Asia/Seoul'}
+                />
               </div>
-            </a>
-          </li>
-          <li className="type-cell">
-            <a
-              href="#!"
-              className="btn-common btn-disabled"
-              onClick={(e) => e.preventDefault}
-            >
-              <span className="menu-title">
-                차량
-                <br />
-                관리
-              </span>
+              <div className="item weather ico-weather01">-3°C</div>
+            </div>
+          </div>
 
-              <div className="icon-box">
-                <i className="ico-update"></i>
-                <i className="ico-bus-admin"></i>
-              </div>
-            </a>
-          </li>
-          <li className="type-cell">
-            <a
-              href="#!"
-              className="btn-common btn-disabled"
-              onClick={(e) => e.preventDefault}
-            >
-              <span className="menu-title">
-                운행
-                <br />
-                분석
-              </span>
+          {/* 
+            1. .btn-common에 추가
+              1) .btn-disabled : 버튼 텍스트 회색 처리, 준비중 추가
+              2) .btn-holiday : 휴무신청기간 추가
+          */}
+          <ul className="menu-list">
+            <li className="type-cell">
+              <a
+                href="/management"
+                className="btn-common"
+                component={busRouteData}
+              >
+                <span className="menu-title">
+                  근무
+                  <br />
+                  일정
+                </span>
 
-              <div className="icon-box">
-                <i className="ico-alarm"></i>
-                <i className="ico-analysis"></i>
-              </div>
-            </a>
-          </li>
-          <li className="type-row">
-            <a href="#!" className="btn-common btn-disabled">
-              <span className="menu-title">공지사항</span>
+                <div className="icon-box">
+                  <i className="ico-alarm on"></i>
+                  <i className="ico-calendar"></i>
+                </div>
+              </a>
+            </li>
+            <li className="type-cell">
+              <a
+                href="/personalTimeTable"
+                className="btn-common"
+                // onClick={() => alert('준비중인 기능입니다.')}
+              >
+                <span className="menu-title">
+                  배차
+                  <br />
+                  일보
+                </span>
 
-              <div className="icon-box">
-                <i className="ico-alarm"></i>
-                <i className="ico-notice"></i>
-              </div>
-            </a>
-          </li>
-          <li className="type-row">
-            <a
-              href="#!"
-              className="btn-common btn-disabled"
-              onClick={(e) => e.preventDefault}
-            >
-              <span className="menu-title">분실물 관리</span>
+                <div className="icon-box">
+                  <i className="ico-alarm"></i>
+                  <i className="ico-bus-run"></i>
+                </div>
+              </a>
+            </li>
+            <li className="type-cell">
+              <a
+                href="#!"
+                className="btn-common btn-disabled"
+                onClick={(e) => e.preventDefault}
+              >
+                <span className="menu-title">
+                  차량
+                  <br />
+                  관리
+                </span>
 
-              <div className="icon-box">
-                <i className="ico-update"></i>
-                <i className="ico-lost-item"></i>
-              </div>
-            </a>
-          </li>
-        </ul>
+                <div className="icon-box">
+                  <i className="ico-update"></i>
+                  <i className="ico-bus-admin"></i>
+                </div>
+              </a>
+            </li>
+            <li className="type-cell">
+              <a
+                href="#!"
+                className="btn-common btn-disabled"
+                onClick={(e) => e.preventDefault}
+              >
+                <span className="menu-title">
+                  운행
+                  <br />
+                  분석
+                </span>
+
+                <div className="icon-box">
+                  <i className="ico-alarm"></i>
+                  <i className="ico-analysis"></i>
+                </div>
+              </a>
+            </li>
+            <li className="type-row">
+              <a href="#!" className="btn-common btn-disabled">
+                <span className="menu-title">공지사항</span>
+
+                <div className="icon-box">
+                  <i className="ico-alarm"></i>
+                  <i className="ico-notice"></i>
+                </div>
+              </a>
+            </li>
+            <li className="type-row">
+              <a
+                href="#!"
+                className="btn-common btn-disabled"
+                onClick={(e) => e.preventDefault}
+              >
+                <span className="menu-title">분실물 관리</span>
+
+                <div className="icon-box">
+                  <i className="ico-update"></i>
+                  <i className="ico-lost-item"></i>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="on">
+              <a href="#!">
+                <span className="blind">홈</span>
+              </a>
+            </li>
+            <li>
+              <a href="/management">
+                <span className="blind">근무관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="#!">
+                <span className="blind">배차일보</span>
+              </a>
+            </li>
+            <li>
+              <a href="#!">
+                <span className="blind">사용자설정</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="on">
-            <a href="#!">
-              <span className="blind">홈</span>
-            </a>
-          </li>
-          <li>
-            <a href="/management">
-              <span className="blind">근무관리</span>
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <span className="blind">배차일보</span>
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <span className="blind">사용자설정</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </>
   );
 };
 
