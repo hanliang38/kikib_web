dd932a249105ebadac712c5628bf6d010f3aa031
mira <mira@miraui-MacBookPro.local>
2022-02-09 17:55:09 +0900

    2022-02-09 | feat(LeaveReqReplaceModal): Mockup

--- src/components/modal/LeaveReqReplaceModal.js
+++ src/components/modal/LeaveReqReplaceModal.js
@@ -1,4 +1,4 @@
-import React /*useState*/ from 'react';
+import React, { useState /*useState*/ } from 'react';
 import '../../css/modal.css';
 import { useLocation } from 'react-router-dom';
 import apiClient from '../../config/apiClient';
@@ -11,67 +11,77 @@ const LeaveReqReplaceModal = (props) => {
   const selectDate = `${dateArr[1]}월 ${dateArr[2]}일`;
   const userInfo = JSON.parse(window.sessionStorage.getItem('userInfo'));
   const myName = userInfo.name;
-  // const
+  //
+  const [selectAngel, setSelectAngel] = useState('승무원');
+  const [selectMyLeave, setSelectMyLeave] = useState('00월00일');
 
   // 1. 선택 가능 자신의 휴무 날짜 (교환할 내 휴무일) (휴무 -> 근무)
   const leaveData = location.state.leaveData;
   // 2. 현재 근무날짜의 WorkId = 휴무일로 바꾸고 싶은 근무일 (근무 -> 휴무)
   const workData = location.state.workList;
-  const allData = location.state.allData;
+  const reqDriverWorkId = workData
+    .filter((item) => item.date === location.state.date)
+    .map((item) => item.id);
+  console.log(reqDriverWorkId);
+
   // 3. 교환하고 싶은 사람의 데이터
   const angel = offList;
-  console.log('angel::', angel);
+  // console.log('angel::', angel);
 
-  console.log(
-    'workData',
-    workData,
-    'leaveData::',
-    leaveData,
-    'allData::',
-    allData
-  );
+  console.log('workData', workData, 'leaveData::', leaveData);
 
-  // // 요청
-  // const fetchData = async () => {
-  //   await apiClient
-  //     .post(`/replace/request`, {
-  //       reqDriverLeaveId: 0,
-  //       reqDriverWorkId: 0,
-  //       resDriverLeaveId: 0,
-  //     })
-  //     .then((res) => {});
-  // };
+  const handleSelectAngel = (e) => {
+    setSelectAngel(e.target.value);
+  };
+  const handleSelectMyLeave = (e) => {
+    setSelectMyLeave(e.target.value);
+  };
+
+  // 요청
+  const fetchData = async () => {
+    await apiClient
+      .post(`/replace/request`, {
+        reqDriverLeaveId: 0,
+        reqDriverWorkId: reqDriverWorkId[0],
+        resDriverLeaveId: 0,
+      })
+      .then((res) => {});
+  };
 
   return (
     <>
       <div className={open ? 'openModal modal' : 'modal'}>
         {open ? (
           <section>
-            <header>
+            <div>
               휴무 교환 신청일
               <br />
               {/* reqDriverWorkId : 쉬고 싶은 날짜 */}
               {selectDate}
-            </header>
+            </div>
             <main>
               <div>
                 <h2>휴무 교환 대상</h2>
                 <form>
                   {/* resDriverLeaveId : 요청할 사람 */}
-                  <select name="replace name">
-                    <option value="none">승무원 선택 ⌵</option>
+                  <select name="replace name" onChange={handleSelectAngel}>
+                    <option value="승무원">승무원 선택 ⌵</option>
                     {angel.map((myAngel, i) => (
-                      <option key={`list-${i}`}>{myAngel.driverName}</option>
+                      <option key={`list-${i}`} value={myAngel.driverName}>
+                        {myAngel.driverName}
+                      </option>
                     ))}
                   </select>
                 </form>
                 <h2>교환할 내 휴무일</h2>
                 <form>
                   {/* requestDriverLeaveId : 기존 휴무일 */}
-                  <select name="replace date">
-                    <option value="none">휴무일 선택 ⌵</option>
+                  <select name="replace date" onChange={handleSelectMyLeave}>
+                    <option value="00월00일">휴무일 선택 ⌵</option>
                     {leaveData.map((myLeave, i) => (
-                      <option key={`list-${i}`}>{myLeave.date}</option>
+                      <option key={`list-${i}`} value={myLeave.date}>
+                        {myLeave.date}
+                      </option>
                     ))}
                   </select>
                 </form>
@@ -82,27 +92,27 @@ const LeaveReqReplaceModal = (props) => {
                 <span>
                   {myName}
                   <br />
-                  {`내 휴무`}
+                  {selectMyLeave}
                 </span>
                 <div>
                   →<br />←
                 </div>
                 <span>
-                  {`천사님`}
+                  {selectAngel}
                   <br />
                   {selectDate}
                 </span>
-                <p>신청하는 정보가 맞습니까?</p>
+                <p>
+                  {selectAngel !== '승무원' && selectMyLeave !== '00월00일'
+                    ? `휴무 교환 정보가 맞습니까?`
+                    : `정보를 입력해주세요`}
+                </p>
               </div>
-              <button>취소</button>
-              <button>확인</button>
-            </main>
-            <footer>
               <button className="close" onClick={close}>
-                {' '}
-                close{' '}
+                취소
               </button>
-            </footer>
+              <button onClick={fetchData}>확인</button>
+            </main>
           </section>
         ) : null}
       </div>
