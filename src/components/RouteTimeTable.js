ad97e0ae733c1fbafe1d83aa05fa68a6fb0041bd
mira <mira@miraui-MacBookPro.local>
2022-02-16 15:20:56 +0900

    2022-02-16 | fix(RouteTimeTable) : useRef추가

--- src/components/RouteTimeTable.js
+++ src/components/RouteTimeTable.js
@@ -1,30 +1,44 @@
-import React from 'react';
-// import styled from 'styled-components';
+import React, { useEffect, useRef, useState } from 'react';
 
 function RouteTimeTable({ busNumber }) {
   const data = require('../assets/GyeonggiBusData/businfo.json');
+  const [headHeight, setHeadHeight] = useState();
 
   const routeData = data[busNumber];
   // console.log(routeData);
   const routeStationData = routeData.stnList;
-  
+
   // table-head 높이값 체크
   // const headHeight = this.divElement.clientHeight;
+  const divElRef = useRef();
+
+  // const styles = { height: 'calc(100% -' + headHeight };
+  useEffect(() => {
+    setHeadHeight(divElRef.current.clientHeight);
+  }, []);
+
+  const styles = { height: `calc(100% - ${headHeight}` };
 
   return (
     <>
       <div className="routetable-box">
-        <div className="table-head">
+        <div className="table-head" ref={divElRef}>
+          {/* <div className="table-head"> */}
           <div>
-            <strong className="point">기점</strong><br/><span className="station">{routeData.info.startStationName}</span>
+            <strong className="point">기점</strong>
+            <br />
+            <span className="station">{routeData.info.startStationName}</span>
           </div>
           <div>
-            <strong className="point">종점</strong><br/><span className="station">{routeData.info.endStationName}</span>
+            <strong className="point">종점</strong>
+            <br />
+            <span className="station">{routeData.info.endStationName}</span>
           </div>
         </div>
 
         {/* style={{height:'calc(100% -' + headHeight}} */}
-        <div className="table-body">
+        <div className="table-body" style={styles}>
+          {/* <div className="table-body"> */}
           <ul>
             {routeStationData.map((row, i) => (
               <li key={`stationList-${i}`}>{row.stationName}</li>
@@ -32,55 +46,8 @@ function RouteTimeTable({ busNumber }) {
           </ul>
         </div>
       </div>
-
-
-      {/* <TableContainer>
-        <TableHead>
-          <TableCell>
-            <HeadTitle>기점</HeadTitle>
-            <HeadContent>{routeData.info.startStationName}</HeadContent>
-          </TableCell>
-          <TableCell>
-            <HeadTitle>종점</HeadTitle>
-            <HeadContent>{routeData.info.endStationName}</HeadContent>
-          </TableCell>
-        </TableHead>
-        <TableBoby>
-          {routeStationData.map((row, i) => (
-            <TableRow key={`stationList-${i}`}>{row.stationName}</TableRow>
-          ))}
-        </TableBoby>
-      </TableContainer> */}
     </>
   );
 }
-
-// const TableContainer = styled.div`
-//   text-align: center;
-//   height: 100vh;
-// `;
-// const TableHead = styled.div`
-//   width: 100%;
-//   height: 10%;
-//   font-size: 5vw;
-// `;
-// const TableCell = styled.div`
-//   display: inline-block;
-//   width: 50%;
-// `;
-// const HeadTitle = styled.div`
-//   display: inline-block;
-//   margin-right: 10px;
-//   color: #7b868c;
-// `;
-// const HeadContent = styled.div`
-//   display: inline-block;
-//   color: black;
-// `;
-// const TableBoby = styled.div``;
-// const TableRow = styled.div`
-//   width: 100%;
-//   font-size: 5vw;
-// `;
 
 export default RouteTimeTable;
